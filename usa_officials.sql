3a59b7a04441ae5fb22f4607af95122f75b8dc4d
Kelley Parker <kelley.parker07@gmail.com>
2023-10-23 09:06:54 -0500

    Update usa_officials.sql
    planning out

--- usa_officials.sql
+++ usa_officials.sql
@@ -1,4 +1,11 @@
-CREATE 
 
+
+-- DATABASES:
+-- 1) Presidents
+-- 2) House of Representatives
+-- 3) Senate
 
 -- TABLES: 
+-- 1) Secretary of State
+-- 2) Secretary of the interior
+-- 3) Secretary of Defense
