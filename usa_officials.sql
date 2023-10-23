c613c72349b490c245cff0f3943d878ccd272e3a
Kelley Parker <kelley.parker07@gmail.com>
2023-10-23 16:36:01 -0500

    Update usa_officials.sql

--- usa_officials.sql
+++ usa_officials.sql
@@ -1,3 +1,88 @@
+CREATE DATABASE usgofficials;
+
+USE usgofficials;
+
+CREATE TABLE presidents {
+  id int PRIMARY KEY AUTO_INCREMENT,
+  full_name VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  birth_year int,
+  political_party, VARCHAR(255),
+};
+
+CREATE TABLE presidents {
+  id int PRIMARY KEY AUTO_INCREMENT,
+  president_id INT,
+  full_name VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  birth_year int,
+  political_party, VARCHAR(255),
+  FOREIGN KEY (president_id) REFERENCES presidents(id)
+};
+
+-- Create a table for chiefs of staff
+CREATE TABLE chiefs_of_staff (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    president_id INT,
+    full_name VARCHAR(255),
+    start_date DATE,
+    end_date DATE,
+    FOREIGN KEY (president_id) REFERENCES presidents(id)
+);
+
+-- Create a table for congress officials
+CREATE TABLE congress_officials (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(255),
+    position VARCHAR(255),
+    start_date DATE,
+    end_date DATE
+);
+
+-- Presidents Values below
+INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  -- George Washington
+  INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  VALUES ('George Washington', '1789-04-30', '1797-03-04', 1732, 'None');
+  
+  -- John Adams
+  INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  VALUES ('John Adams', '1797-03-04', '1801-03-04', 1735, 'Federalist');
+  
+  -- Thomas Jefferson
+  INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  VALUES ('Thomas Jefferson', '1801-03-04', '1809-03-04', 1743, 'Democratic-Republican');
+  
+  -- James Madison
+  INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  VALUES ('James Madison', '1809-03-04', '1817-03-04', 1751, 'Democratic-Republican');
+  
+  -- James Monroe
+  INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  VALUES ('James Monroe', '1817-03-04', '1825-03-04', 1758, 'Democratic-Republican');
+  
+  -- John Quincy Adams
+  INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  VALUES ('John Quincy Adams', '1825-03-04', '1829-03-04', 1767, 'Democratic-Republican');
+  
+  -- Andrew Jackson
+  INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  VALUES ('Andrew Jackson', '1829-03-04', '1837-03-04', 1767, 'Democratic');
+  
+  -- Martin Van Buren
+  INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  VALUES ('Martin Van Buren', '1837-03-04', '1841-03-04', 1782, 'Democratic');
+  
+  -- William Henry Harrison
+  INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  VALUES ('William Henry Harrison', '1841-03-04', '1841-04-04', 1773, 'Whig');
+  
+  -- John Tyler
+  INSERT INTO presidents (full_name, start_date, end_date, birth_year, political_party)
+  VALUES ('John Tyler', '1841-04-04', '1845-03-04', 1790, 'Whig')
+);
 
 
 -- DATABASES:
